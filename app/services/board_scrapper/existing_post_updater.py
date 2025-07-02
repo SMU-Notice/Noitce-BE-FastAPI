37f3e9cb5b6c547a723e203c86f68e7c9ac68653
jimmy <hojun.korea@gmail.com>
2025-07-03 01:10:17 +0900

    refactor : 과거 아키텍쳐 불필요한 파일 삭제

--- app/services/board_scrapper/existing_post_updater.py
+++
@@ -1,6 +0,0 @@
-class ExistingPostsUpdater:
-    def __init__(self, post_repo: PostRepository):
-        self.post_repo = post_repo
-
-    async def update(self, existing_posts_id_view_count: list[dict]):
-        await self.post_repo.update_multiple_posts(existing_posts_id_view_count)
